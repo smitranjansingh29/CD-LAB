47f6190c21da3371bbc0bdc1e5367fd15b65292c
smitranjansingh29 <62330908+smitranjansingh29@users.noreply.github.com>
2021-01-02 23:10:32 +0530

    Create operator_parsing_table.c

---
+++ operator_parsing_table.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void f()
+{
+    printf("Not operator precedence grammar");
+    exit(0);
+}
+
+void main()
+{
+
+    printf("SHREY MIDHA\n");
+    printf("1816110197\n");
+    char grm[20][20], c;
+
+    int i, n, j = 2, flag = 0;
+    printf("Enter the length:");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%s", grm[i]);
+
+    for (i = 0; i < n; i++)
+    {
+        c = grm[i][2];
+
+        while (c != '\0')
+        {
+
+            if (grm[i][3] == '+' || grm[i][3] == '-' || grm[i][3] == '*' || grm[i][3] == '/')
+
+                flag = 1;
+
+            else
+            {
+
+                flag = 0;
+                f();
+            }
+
+            if (c == '$')
+            {
+                flag = 0;
+                f();
+            }
+
+            c = grm[i][++j];
+        }
+    }
+
+    if (flag == 1)
+        printf("Operator precedence grammar");
+}
